9daf5554c5ae394923bc314dace2a1ae10cb5599
Taehwan.Kim <seumoon@gmail.com>
2021-08-30 16:59:17 +1000

    Determinant function added

--- Main.py
+++ Main.py
@@ -1,7 +1,12 @@
 from DotProduct import dot_product
+from Determinant import determinant
 
-function_type = int(input("Type 1 for DotProduct: "))
+function_type = int(input("Type 1 for DotProduct, 2 for determinant: "))
 if function_type == 1:
     dot_product()
+
+elif function_type == 2:
+    determinant()
+
 else:
     raise ValueError("Not a proper number")
